abd1da0ca46beda1a1a68502a9a65e7c56b81106
timi salminen <salminen.timi@gmail.com>
2024-07-18 20:47:34 +0300

    siirry 3d aikaan

--- scriptit/Hytti.gd
+++ scriptit/Hytti.gd
@@ -5,17 +5,17 @@ var kuopat
 var kuoppa
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	heilunta += delta 
-	kuoppa = randf_range(0,1)
-	if kuoppa <= 0.17:
-		kuopat = randf_range(0,0.2)
-		heilunta -= kuopat
-	position.y = 300 + 30*sin(heilunta)
-	if kuoppa <= 0.17:
-		heilunta += kuopat
-	pass
+    heilunta += delta 
+    kuoppa = randf_range(0,1)
+    if kuoppa <= 0.17:
+        kuopat = randf_range(0,0.2)
+        heilunta -= kuopat
+    position.y = 300 + 5*sin(heilunta)
+    if kuoppa <= 0.17:
+        heilunta += kuopat
+    pass
--- scriptit/player.gd
+++ scriptit/player.gd
@@ -6,13 +6,13 @@ const max_speed = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_key_pressed(turn_left):
-		rotation = rotation-max_steer
-	if Input.turn_right():
-		rotation = rotation+max_steer
-	pass
+    if Input.is_key_pressed(turn_left):
+        rotation = rotation-max_steer
+    if Input.turn_right():
+        rotation = rotation+max_steer
+    pass
