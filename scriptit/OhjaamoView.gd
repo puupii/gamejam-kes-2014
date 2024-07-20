d4f0110c70fd6a37a8fe2066607813713c3a1a5f
ropo <github@roponkolo.fi>
2024-07-20 21:06:25 +0300

    ohjaamoa ja shadereitä sörkitty

--- scriptit/OhjaamoView.gd
+++ scriptit/OhjaamoView.gd
@@ -5,7 +5,12 @@ var speed = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+<<<<<<< HEAD
     pass # Replace with function body.
+=======
+	self.visible = false
+	pass # Replace with function body.
+>>>>>>> b062db8 (shaderiä ja ohjaamoa sörkitty)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
