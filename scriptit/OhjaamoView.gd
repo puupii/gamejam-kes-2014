da39f88c698e185d693a9936577ae1a9fc28b3d1
ropo <github@roponkolo.fi>
2024-07-20 21:15:57 +0300

    rebase korjattu

--- scriptit/OhjaamoView.gd
+++ scriptit/OhjaamoView.gd
@@ -5,14 +5,11 @@ var speed = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-<<<<<<< HEAD
-    pass # Replace with function body.
-=======
 	self.visible = false
 	pass # Replace with function body.
->>>>>>> b062db8 (shaderiä ja ohjaamoa sörkitty)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    position.z += speed * delta
+	position.z += speed * delta
