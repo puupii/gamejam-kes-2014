cefc61d3f25a351be4f34cab05a03b08cec9d6f5
ropo <github@roponkolo.fi>
2024-07-20 12:47:32 +0300

    ohjaamo scene lisätty 3d sceneen

--- scriptit/OhjaamoView.gd
+++ scriptit/OhjaamoView.gd
@@ -4,9 +4,9 @@ var speed = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    position.z += speed * delta
+	position.z += speed * delta
