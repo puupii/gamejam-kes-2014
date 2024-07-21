fcdf8a7b8570f9f9e2b17da609c1609db6b68a28
timi salminen <salminen.timi@gmail.com>
2024-07-21 10:52:46 +0300

    kamera hajos

---
+++ scriptit/Camera3D.gd
@@ -0,0 +1,15 @@
+extends Camera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
+
+func _input(event):
+    if Input.is_key_pressed(KEY_E):
+        rotate_y(0.1)
