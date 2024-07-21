3045c548f9f9ad5056721faa09be483ad4ded2ce
timi salminen <salminen.timi@gmail.com>
2024-07-21 20:17:14 +0300

    ui uudelleen sijoitettu

--- scriptit/inventory.gd
+++ scriptit/inventory.gd
@@ -7,17 +7,17 @@ const position_out = Vector2(400,150)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 
 func slideout():
-	self.position = position_in.lerp(position_out,0.5)
-	
+    self.position = position_in.lerp(position_out,0.5)
+    
 
 func slidein():
-	self.position = position_out.lerp(position_in,0.5)
+    self.position = position_out.lerp(position_in,0.5)
