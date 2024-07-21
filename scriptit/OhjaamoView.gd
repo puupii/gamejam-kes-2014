be04ee6246b878893a4423c32f39eb68bccba45f
psycoma <timi_salminen@hotmail.com>
2024-07-21 11:44:04 +0300

    korjattu kamera hässäkkä

--- scriptit/OhjaamoView.gd
+++ scriptit/OhjaamoView.gd
@@ -1,15 +1,11 @@
 extends MeshInstance3D
 
-@export var OhjaamoView : Node
 var speed = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.visible = false
-	pass # Replace with function body.
-
-
+    pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.z += speed * delta
+    position.z += speed * delta
