d6815b9a55e8ce4e4e1463b5fd5307348dc6534c
ropo <github@roponkolo.fi>
2024-07-23 18:31:20 +0300

    radion toiminta nyt AudioServer.set_bus kautta

--- scriptit/Hytti.gd
+++ scriptit/Hytti.gd
@@ -3,19 +3,47 @@ extends Sprite2D
 var heilunta = 0.0
 var kuopat
 var kuoppa
+var radiotimer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	radiotimer = 0.0
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    heilunta += delta 
-    kuoppa = randf_range(0,1)
-    if kuoppa <= 0.17:
-        kuopat = randf_range(0,0.2)
-        heilunta -= kuopat
-    position.y = 300 + 5*sin(heilunta)
-    if kuoppa <= 0.17:
-        heilunta += kuopat
-    pass
+	#hytin heilunta
+	heilunta += delta 
+	kuoppa = randf_range(0,1)
+	if kuoppa <= 0.17:
+		kuopat = randf_range(0,0.2)
+		heilunta -= kuopat
+	position.y = 300 + 5*sin(heilunta)
+	if kuoppa <= 0.17:
+		heilunta += kuopat
+		
+	#radio sammuu automaattisesti 30sec kuluttua
+	if radiotimer < 0.0:
+		AudioServer.set_bus_mute(2, true)
+	if radiotimer > 0.0:
+		radiotimer -= delta
+	
+
+################################################################################
+############################# Radion toiminta ##################################
+################################################################################
+func _on_radio_pressed():
+		if $RadioSoitin.is_playing() != true:
+			AudioServer.set_bus_volume_db(1,-8.0)
+			AudioServer.set_bus_mute(2, false)
+			radiotimer = 30.0
+			#valittuKappale = Musiikki1
+			#$RadioSoitin.stream = valittuKappale
+			$RadioSoitin.play()
+		
+		elif $RadioSoitin.is_playing() == true:
+			radiotimer = 0
+			AudioServer.set_bus_mute(2, true)
+			$RadioSoitin.stop()
+			AudioServer.set_bus_volume_db(1,0.0)
+################################################################################
--- scriptit/maailma.gd
+++ scriptit/maailma.gd
@@ -3,75 +3,61 @@ extends Node2D
 var nykyinenNakyma = "etu"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
-    $Etunakyma.show()
-    $LepotilaNakyma.hide()
-    $ApukuskinpuolenNakyma.hide()
-    $KuskinpuolenNakyma.hide()
+	pass # Replace with function body.
+	$Etunakyma.show()
+	$LepotilaNakyma.hide()
+	$ApukuskinpuolenNakyma.hide()
+	$KuskinpuolenNakyma.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 ################################################################################
 #################### kameran kääntäminen scenejen välillä ######################
 ################################################################################
 
 func _on_left_arrow_pressed():
-    if nykyinenNakyma == "etu":
-        nykyinenNakyma = "vasen"
-        $Etunakyma.hide()
-        $KuskinpuolenNakyma.show()
-        
-    elif nykyinenNakyma == "vasen":
-        nykyinenNakyma = "taka"
-        $KuskinpuolenNakyma.hide()
-        $LepotilaNakyma.show()
-        
-    elif nykyinenNakyma == "taka":
-        nykyinenNakyma = "oikea"
-        $LepotilaNakyma.hide()
-        $ApukuskinpuolenNakyma.show()
-        
-    else:
-        nykyinenNakyma = "etu"
-        $ApukuskinpuolenNakyma.hide()
-        $Etunakyma.show()
+	if nykyinenNakyma == "etu":
+		nykyinenNakyma = "vasen"
+		$Etunakyma.hide()
+		$KuskinpuolenNakyma.show()
+		
+	elif nykyinenNakyma == "vasen":
+		nykyinenNakyma = "taka"
+		$KuskinpuolenNakyma.hide()
+		$LepotilaNakyma.show()
+		
+	elif nykyinenNakyma == "taka":
+		nykyinenNakyma = "oikea"
+		$LepotilaNakyma.hide()
+		$ApukuskinpuolenNakyma.show()
+		
+	else:
+		nykyinenNakyma = "etu"
+		$ApukuskinpuolenNakyma.hide()
+		$Etunakyma.show()
 
 
 func _on_right_arrow_pressed():
-    if nykyinenNakyma == "etu":
-        nykyinenNakyma = "oikea"
-        $Etunakyma.hide()
-        $ApukuskinpuolenNakyma.show()
-        
-    elif nykyinenNakyma == "oikea":
-        nykyinenNakyma = "taka"
-        $ApukuskinpuolenNakyma.hide()
-        $LepotilaNakyma.show()
-        
-    elif nykyinenNakyma == "taka":
-        nykyinenNakyma = "vasen"
-        $LepotilaNakyma.hide()
-        $KuskinpuolenNakyma.show()
-        
-    else:
-        nykyinenNakyma = "etu"
-        $KuskinpuolenNakyma.hide()
-        $Etunakyma.show()
+	if nykyinenNakyma == "etu":
+		nykyinenNakyma = "oikea"
+		$Etunakyma.hide()
+		$ApukuskinpuolenNakyma.show()
+		
+	elif nykyinenNakyma == "oikea":
+		nykyinenNakyma = "taka"
+		$ApukuskinpuolenNakyma.hide()
+		$LepotilaNakyma.show()
+		
+	elif nykyinenNakyma == "taka":
+		nykyinenNakyma = "vasen"
+		$LepotilaNakyma.hide()
+		$KuskinpuolenNakyma.show()
+		
+	else:
+		nykyinenNakyma = "etu"
+		$KuskinpuolenNakyma.hide()
+		$Etunakyma.show()
 ################################################################################
 
-################################################################################
-############################# Radion toiminta ##################################
-################################################################################
-func _on_radio_pressed():
-        if $RadioSoitin.is_playing() != true:
-            $rekanÄäni.set_volume_db(-15.0)
-            #valittuKappale = Musiikki1
-            #$RadioSoitin.stream = valittuKappale
-            $RadioSoitin.play()
-        
-        elif $RadioSoitin.is_playing() == true:
-            $RadioSoitin.stop()
-            $rekanÄäni.set_volume_db(1.0)
-################################################################################
